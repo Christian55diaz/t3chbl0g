e28c5ff8ee46f5d2e54a6c4d7ae7cbea9009efa7
Christian Diaz <christian55diaz@gmail.com>
2022-03-08 23:15:19 -0500

    refractored delete-comment

---
+++ public/js/delete-comment.js
@@ -0,0 +1,27 @@
+//this is for deleting a comment
+    async function deleteCommentFormHandler(event) {
+    event.preventDefault();
+//this is to delete the whole blog post i think
+//bc I am not sure how to delete the individual blogpost by id
+const blogpost_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+//console log by blogpost id
+console.log(blogpost_id);
+  const response = await fetch(`/api/comments/${blogpost_id}`, {
+    method: "DELETE",
+    body: JSON.stringify({
+      blogpost_id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  //response + alert
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
+  }
+}
