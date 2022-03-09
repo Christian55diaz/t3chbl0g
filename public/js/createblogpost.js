fc55023dad0d5c63d9e2bbeac31e8ae0b67ceb8c
Christian Diaz <christian55diaz@gmail.com>
2022-03-08 22:57:25 -0500

    added post method + if statement

--- public/js/createblogpost.js
+++ public/js/createblogpost.js
@@ -4,3 +4,24 @@ async function createFormHandler(event) {
 //const for title and actually content of the blog post
     const title = document.querySelector(".blog-title").value;
     const blog_content = document.querySelector(".blog-body").value;
+
+    //we are trying to get response from our blogpost api
+    //post method for title and blog content
+    const response = await fetch(`/api/blogpost`, {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          blog_content,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      //if statment for the user dashboard
+      if (response.ok) {
+        document.location.replace("/dashboard");
+      } else {
+        alert(response.statusText);
+      }
+    }
