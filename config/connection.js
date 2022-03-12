fdcaecfdfdaa2b7b33ce0616055c40aafffb53fe
Christian Diaz <christian55diaz@gmail.com>
2022-03-12 14:51:56 -0500

    one to many + one to one relationships

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
+//require sequalize and dotenv file
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
+// for dotenv reasons/layout
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  //how to connect to db
   {
     host: 'localhost',
     dialect: 'mysql',
--- models/index.js
+++ models/index.js
@@ -4,10 +4,27 @@ const Blog = require('./Blog');
 const Comment = require('./Comment');
 
 //here we are going to make one to one and one to many relationships
-
+//one to many
 User.hasMany(Blog, {
     foriegnkey: 'user_id'
 });
+//one to one
 Blog.belongsTo(User, {
     foriegnkey: 'user_id'
+});
+//one to one
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+//one to one
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id', 
+});
+//one to many
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+  });
+  //one to many
+  Blog.hasMany(Comment, {
+    foreignKey: 'post_id'
 });
