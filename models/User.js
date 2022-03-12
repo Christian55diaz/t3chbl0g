35b138b1018d9e550c3d2f3fa90d22bbb63500f3
Christian Diaz <christian55diaz@gmail.com>
2022-03-12 15:07:13 -0500

    async/await functions added

--- models/User.js
+++ models/User.js
@@ -28,5 +28,34 @@ User.init(
             autoIncrement: true
         },
         //lets make a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          //password column
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              // length of password must be at least 8 characters
+              len: [8]
+            }
+        }
+    },
+    {
+        //async/await function
+        //hash everytime user is created to save for later
+        hooks: {
+            //beforeCreate setup
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+              },
+              //beforeUpdate
+              async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        }
     }
 )
