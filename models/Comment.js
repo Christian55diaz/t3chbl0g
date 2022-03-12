5b572295a14627fb67b029765ce6afee0d4d1755
Christian Diaz <christian55diaz@gmail.com>
2022-03-12 15:15:53 -0500

    comment model created

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,11 @@
-//comment model
+//comment model requirements
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+//comment model created
+class Comment extends Model {}
+
+Comment.init(
+    {
+        
+    }
+)
