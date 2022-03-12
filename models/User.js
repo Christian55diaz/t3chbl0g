e6af8470c3c16c196851f6b11e67e51e5074da1a
Christian Diaz <christian55diaz@gmail.com>
2022-03-12 14:56:27 -0500

    user requirements added plus model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,14 @@
 //user model
+//require bcrypt for hasing library
+const bcrypt = require('bcrypt');
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection'); 
+
+//user model which is also used for user table
+class User extends Model {
+    //method to run(per user) to check passwords
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
