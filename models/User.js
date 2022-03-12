c2b1cb5504ec5e2a172b1528ca4a37fce9987ba6
Christian Diaz <christian55diaz@gmail.com>
2022-03-12 15:08:11 -0500

    sequalized User

--- models/User.js
+++ models/User.js
@@ -56,6 +56,11 @@ User.init(
                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
                 return updatedUserData;
             }
-        }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
     }
 )
