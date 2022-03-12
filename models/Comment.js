08c4da654d58f09e42cea41b1ce1eeda71df7b39
Christian Diaz <christian55diaz@gmail.com>
2022-03-12 15:20:40 -0500

    id for comment and user added to models

--- models/Comment.js
+++ models/Comment.js
@@ -6,6 +6,22 @@ class Comment extends Model {}
 
 Comment.init(
     {
+        //id of the comment
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          //comment from the user
+          user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: "user",
+              key: "id",
+            },
+        },
         
     }
 )
