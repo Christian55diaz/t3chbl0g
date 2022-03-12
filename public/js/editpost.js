7f77c75a0c1ee07794fd9b2ff0b4b1f7b9657a83
Christian Diaz <christian55diaz@gmail.com>
2022-03-12 03:12:16 -0500

    comments were added

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -1,13 +1,13 @@
-//
+//edit form handle
 async function editFormHandler (event) {
     event.preventDefault();
-
+// const or require id and title b/c we update the post by id by specific title & id
     const title = document.querySelector('input[name="post-title]').value;
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
     const contents = document.querySelector('textarea[name="post-contents"]').value.trim();
-
+//method to allow for put/update/edit to go through properly
     if (title !== "" && contents !== "") {
         const response = await fetch(`/api/posts/${id}`, {
             method: 'PUT',
@@ -24,6 +24,7 @@ async function editFormHandler (event) {
             } else {
                 alert(response.statusText);
             }
+            //if statements so that the edit proccess goes smoothly
     } else {
         if (title === "" && contents === "") {
             alert("Title and description required")
