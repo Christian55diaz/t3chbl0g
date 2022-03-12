5ae61ed80002a4a678ccadbfeee8bfdc59bae319
Christian Diaz <christian55diaz@gmail.com>
2022-03-12 03:00:37 -0500

    function for editing added + setup

---
+++ public/js/editpost.js
@@ -0,0 +1,14 @@
+//
+async function editFormHandler (event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const contents = document.querySelector('textarea[name="post-contents"]').value.trim();
+
+    if (title !== "" && contents !== "") {
+        
+    }
+}
