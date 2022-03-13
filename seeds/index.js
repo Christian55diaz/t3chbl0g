edce5ddec3e6e927cf98f8fff29f0d598edda03b
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 04:09:14 -0400

    requirements added

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,6 @@
 //index for seeds
+//setup consts
+const sequelize= require('../config/connection');
+const seedComment= require('./commentData');
+const seedBlog= require('./blogData');
+const seedUser= require('./userData');
