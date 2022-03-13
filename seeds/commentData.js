1c43d1dff43f64af3b51529ba90726a7f27e51f1
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 04:24:09 -0400

    comments made

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -3,6 +3,26 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        
+      user_id: 1,
+      blogpost_id: 1,
+      comment_text: "hope everyone enjoy the post"
+    },
+    {
+      user_id: 3,
+      blogpost_id: 2,
+      comment_text: "WOWZERS"
+    },
+    {
+      user_id: 2,
+      blogpost_id: 3,
+      comment_text: "fortnite battlepass"
+    },
+    {
+      user_id: 1,
+      blogpost_id: 4,
+      comment_text: "chilll bro do not do that now"
     }
-]
+  ];
+
+  //bulkcreate commentData
+  
