bbcb3ede666fba44804dde269d1f3233b831d9bd
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 04:00:49 -0400

    adding blog init code

--- models/Blog.js
+++ models/Blog.js
@@ -2,4 +2,23 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 //blog model created
-class BlogPost extends Model {}
+class Blog extends Model {}
+
+Blog.init(
+    {
+        //blog id
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+            //cascade means child data gets deleted when the parent data is deleted
+            onDelete: 'CASCADE'
+          },
+          //title of blog post
+          title: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+    }
+)
