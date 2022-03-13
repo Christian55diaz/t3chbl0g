92012956b6476d396baa7e79b07f4e5ead73f36b
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 04:10:19 -0400

    seeds setup

--- seeds/index.js
+++ seeds/index.js
@@ -4,3 +4,7 @@ const sequelize= require('../config/connection');
 const seedComment= require('./commentData');
 const seedBlog= require('./blogData');
 const seedUser= require('./userData');
+
+//seeding all the models
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
