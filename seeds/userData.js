7250660b1fa313b5a7c4ea0c5250fd8431a667e9
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 04:19:43 -0400

    setup user accounts

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,1 +1,27 @@
 //users seeds
+//const the models
+const { User } = require('../models');
+
+//creating user data
+const userData = [
+    {
+      username: 'Christian',
+      email: 'christian@gmail.com',
+      password: 'jimmygotbuckets1'
+    },
+    {
+      username: 'SendBandz',
+      email: 'sendbandz@gmail.com',
+      password: 'SendBandz355'
+    },
+    {
+      username: 'Motel8',
+      email: 'motel8@gmail.com',
+      password: "motel8isme"
+    },
+    {
+      username: 'Therapperdrake',
+      email: 'therapperdrake@gmail.com',
+      password: 'therapperdrake908'
+    }
+  ];
