dc20a313958cc30720f69104ba3595f2fea9eacd
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 23:33:38 -0400

    router post find one added

--- controllers/api/blogroutes.js
+++ controllers/api/blogroutes.js
@@ -33,3 +33,47 @@ router.get("/", (req, res) => {
         res.status(500).json(err);
       });
   });
+  //we are finding one specific post by its id
+  router.get("/:id", (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "created_at", "blog_content"],
+      include: [
+         // we need both comments and user because they are associated with blog post
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "blog_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    })
+      //  catching error in the database if needed
+      .then((dbData) => {
+        if (!dbData) {
+          res
+            .status(404)
+            .json({ message: "no blog post found with this id" });
+          return;
+        }
+        res.json(dbData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
