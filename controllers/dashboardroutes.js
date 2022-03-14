9021f782d95cee28ffcccdb486931e9585f4b9c0
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 22:35:34 -0400

    router.get login and signup

--- controllers/dashboardroutes.js
+++ controllers/dashboardroutes.js
@@ -23,3 +23,36 @@ router.get("/", (req, res) => {
           },
         ],
       })
+       // dbData is b/c I am using multiple models at once
+    .then((dbData) => {
+        const Blog = dbData.map((Blog) => Blog.get({ plain: true }));
+        res.render("homepage", {
+          Blog,
+          loggedIn: req.session.loggedIn,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  //router.get login
+  router.get("/login", (req, res) => {
+    // already loggedin go to homepage
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("login");
+  });
+  //router.get signup
+  router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("signup");
+  });
+  //findone blogpost by id
