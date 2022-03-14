43630cfbf14037250722ad7297d80fe0e0d8587f
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 23:00:17 -0400

    refractored

--- controllers/api/userroutes.js
+++ controllers/api/userroutes.js
@@ -2,3 +2,52 @@
 const router = require("express").Router();
 const { User, Blog, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+// get allusers
+router.get("/", (req, res) => {
+    User.findAll({
+      attributes: { exclude: ["password"] },
+    })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  // get user by its id
+  router.get("/:id", (req, res) => {
+    User.findOne({
+      attributes: { exclude: ["password"] },
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: BlogPost,
+          attributes: ["id", "title", "blog_content", "created_at"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "created_at"],
+          include: {
+            model: Post,
+            attributes: ["title"],
+          },
+        },
+      ],
+    })
+    //if error then
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "user not found with id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
