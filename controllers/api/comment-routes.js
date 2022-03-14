5c9b7f91f9139248358211c37fc88cacf4bab42c
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 23:21:45 -0400

    post and getall routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,4 +12,20 @@ router.get("/", (req, res) => {
         res.status(500).json(err);
       });
   });
-  //post comment route
+  //post comment router
+router.post("/", withAuth, (req, res) => {
+    // is user still logged into session
+    if (req.session) {
+      Comment.create({
+        comment_text: req.body.comment_text,
+        blogpost_id: req.body.blogpost_id,
+        // use the user's id from the session
+        user_id: req.session.user_id,
+      })
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    }
+  });
