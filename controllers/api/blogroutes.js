c537f3a4ab44d683d1868204cc3df3d044747fb3
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 23:25:46 -0400

    comments deleted and exported

--- controllers/api/blogroutes.js
+++ controllers/api/blogroutes.js
@@ -2,3 +2,34 @@
 const router = require("express").Router();
 const { Blog, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+//get everything and anything that has to do with blog post
+router.get("/", (req, res) => {
+    Blog.findAll({
+      attributes: ["id", "title", "created_at", "blog_content"],
+      order: ["created_at"],
+      include: [
+        // we need both comments and user because they are associated with blog post
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "blog_id",
+            "user_id",
+            "created_at",
+          ],
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    })
+      //   catching error in the database if needed
+      .then((dbData) => res.json(dbData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -29,3 +29,27 @@ router.post("/", withAuth, (req, res) => {
         });
     }
   });
+
+  //to delete the comments here
+  router.delete("/:id", withAuth, (req, res) => {
+    Comment.destroy({
+      where: {
+        blogpost_id: req.params.id,
+      },
+    })
+      .then((dbCommentData) => {
+        if (!dbCommentData) {
+          res
+            .status(404)
+            .json({ message: "no comment found with id" });
+          return;
+        }
+        res.json(dbCommentData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  // exporting comment router
+  module.exports = router;
