469f6c35c74a04745292470be8d68d655226c010
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 23:08:35 -0400

    added css to button + userfind one route

--- controllers/api/userroutes.js
+++ controllers/api/userroutes.js
@@ -50,4 +50,53 @@ router.get("/", (req, res) => {
         res.status(500).json(err);
       });
   });
+  //signup routes
+  //most code used from a group project i did called mus-it
+  router.post("/", (req, res) => {
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    }).then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
   
+        res.json(dbUserData);
+      });
+    });
+  });
+  //login route
+  //same here used code from previous projects
+  router.post("/login", (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "user not found with id" });
+          return;
+        }
+        const validPW = dbUserData.checkPassword(req.body.password);
+  
+        if (!validPW) {
+          res.status(400).json({ message: "password is invalid" });
+          return;
+        }
+        // this just saves the data you need to the cookie that I set in the beggining of the start of the server
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+  
+          res.json({ user: dbUserData, message: "Welcome, you are logged-in!" });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
