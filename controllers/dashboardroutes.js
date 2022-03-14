86483921855f048f6dd43e4d58bf67e1e14e98e8
Christian Diaz <christian55diaz@gmail.com>
2022-03-13 22:33:10 -0400

    using other projects as a refrence for routes

--- controllers/dashboardroutes.js
+++ controllers/dashboardroutes.js
@@ -1,4 +1,25 @@
 // importing the requirements
 const router = require("express").Router();
-const { BlogPost, User, Comment } = require("../models");
+const { Blog, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
+// trying to finall blogpost routes
+router.get("/", (req, res) => {
+    Blog.findAll({
+      attributes: ["id", "title", "created_at", "blog_content"],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "blog_id",
+            "user_id",
+            "created_at",
+          ],
+        },
+        {
+            model: User,
+            attributes: ["username"],
+          },
+        ],
+      })
